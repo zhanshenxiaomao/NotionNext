9207df2f569f006b1dc6aac1083b72ae4136a1d2
战神小猫 <146637870+zhanshenxiaomao@users.noreply.github.com>
2024-02-15 18:23:57 +0800

    更新 blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -224,7 +224,7 @@ const BLOG = {
     {
       name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      url: 'https://resource.xiaomao.cloudns.org/Seasons (Futuristik & Whogaux Remix).mp3/',
       cover:
             'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
